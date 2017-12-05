943988cd366d2651c07635ed133a71e463bfd0a5
syoyu <syo.kyusyu.0725@gmail.com>
2017-12-05 21:24:23 +0900

    build

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -6,17 +6,17 @@ Vue.component('calendar', {
   data: function() {
       return {
         demoEvents: [{
-          date: '2016/11/12', // Required
+          date: '2017/12/5', // Required
           title: 'Foo' // Required
         }, {
-          date: '2016/12/15',
+          date: '2017/12/15',
           title: 'Bar',
           desc: 'description',
           customClass: 'disabled highlight' // Custom classes to an calendar cell
         }]
       }
   },
-  template: `<vue-event-calendar :events="demoEvents"></vue-event-calendar>`
+  template: `<vue-event-calendar :title="title" :events="demoEvents" @dayChanged="handleDayChange"></vue-event-calendar>`
 })
 
 </script>
