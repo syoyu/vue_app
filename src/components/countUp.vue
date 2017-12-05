9d0f986b7caaf668e404bfb7c6ece6cb123ccdb5
syoyu <syo.kyusyu.0725@gmail.com>
2017-12-05 20:49:38 +0900

    add countUp

---
+++ src/components/countUp.vue
@@ -0,0 +1,33 @@
+<template>
+
+<div id="countUp">
+	<div v-text="count"></div>
+</div>
+	
+</template>
+
+<script>
+
+import Vue from 'vue';
+
+Vue.component('countUp', {    
+  data: function() {
+      return {
+        count: 0
+      }
+  },
+  template: `<div>
+  				<div v-text="count"></div>
+  				<button @click="countup">Up</button>
+			</div>`,
+  methods:{
+  	countup: function(){
+  		this.count++;
+  	}
+  }
+})
+
+	  
+  
+
+</script>
