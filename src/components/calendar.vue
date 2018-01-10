49d18df798d436e4c5501d4fa16ed20dd705a67b
syoyu <syo.kyusyu.0725@gmail.com>
2018-01-11 05:22:13 +0900

    calendar_visual
    カレンダーの表示成功
    welcome homeとpracticeは入れたまま

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,22 +1,21 @@
+<template>
+  <vue-event-calendar :events="demoEvents"></vue-event-calendar>
+</template>
+
 <script>
-
-import Vue from 'vue';
-
-Vue.component('calendar', {
-  data: function() {
-      return {
-        demoEvents: [{
-          date: '2017/12/5', // Required
-          title: 'Foo' // Required
-        }, {
-          date: '2017/12/15',
-          title: 'Bar',
-          desc: 'description',
-          customClass: 'disabled highlight' // Custom classes to an calendar cell
-        }]
-      }
-  },
-  template: `<vue-event-calendar :title="title" :events="demoEvents" @dayChanged="handleDayChange"></vue-event-calendar>`
-})
-
+export default {
+  data () {
+    return {
+      demoEvents: [{
+        date: '2018/1/10', // Required
+        title: 'Foo' // Required
+      }, {
+        date: '2018/1/15',
+        title: 'Bar',
+        desc: 'description',
+        customClass: 'disabled highlight' // Custom classes to an calendar cell
+      }]
+    }
+  }
+}
 </script>
--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -8,9 +8,8 @@
       </p>
 
 <!-- カレンダー -->
-
         <practice></practice>
-        <!-- <calendar></calendar> -->
+        <calendar></calendar>
 
 
     </v-ons-page>
@@ -22,7 +21,13 @@
 <script>
   import Vue from 'vue'
   import customToolbar from './toolbar'
+// 練習用
   import Practice from './practice.vue'
+
+// カレンダーインポート
+  import 'vue-event-calendar/dist/style.css'
+  import vueEventCalendar from 'vue-event-calendar'
+  import Calendar from './calendar.vue'
   // import Calendar from './m_calendar.vue'
 
   export default {
@@ -30,7 +35,9 @@
     components: { customToolbar }
   }
 
+//コンポーネントを登録
   Vue.component('practice',Practice)
-  // Vue.component('calendar',Calendar)
+  Vue.component('calendar',Calendar)
+  Vue.use(vueEventCalendar, {locale: 'en'})
 
 </script>
