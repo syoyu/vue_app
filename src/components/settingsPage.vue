71b87dfb1373c9eac8877054871c35565cb1dee1
syoyu <syo.kyusyu.0725@gmail.com>
2018-01-19 00:13:56 +0900

    addPlan_in_settingPage
    addPlanを作った
    settingPageにリスト表示と追加のインプット
    list.vueのデータとりんくさせたい

--- src/components/settingsPage.vue
+++ src/components/settingsPage.vue
@@ -4,11 +4,18 @@
     <p style="text-align: center">
       Change the settings.
     </p>
+
+    <add></add>
   </v-ons-page>
 </template>
 
 <script>
+  import Vue from 'vue'
   import customToolbar from './toolbar'
+  import Add from './addPlan.vue'
+
+  Vue.component('add' , Add)
+
   export default {
     props: ['toggleMenu'],
     components: { customToolbar }
